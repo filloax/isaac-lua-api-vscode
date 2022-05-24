41ee6ed9ddcea384b39e6b6656bd43f43e048613
Filloax <filloax@gmail.com>
2022-05-24 12:01:19 +0200

    extension works!

--- src/extension.ts
+++ src/extension.ts
@@ -1,12 +1,63 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import * as path from "path";
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+// Used https://github.com/ManticoreGamesInc/vscode-core 
+// (with MIT license) as reference for how to do this
+
 export function activate(context: vscode.ExtensionContext) {
-
+    const emmyluaPath = path.join("out", "emmylua");
+    setExternalLibrary(context, emmyluaPath, true);
+    updateConfig();
 }
 
-// this method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate(context: vscode.ExtensionContext) {
+    const emmyluaPath = path.join("out", "emmylua");
+    setExternalLibrary(context, emmyluaPath, false);
+}
+
+export function setExternalLibrary(context: vscode.ExtensionContext, folder: string, enable: boolean) {
+    console.log("setExternalLibrary", folder, enable);
+
+    const extensionId = context.extension.id;
+    const extensionPath = context.extension.extensionPath;
+    const folderPath = extensionPath ? path.join(extensionPath, folder) : "";
+    const config = vscode.workspace.getConfiguration("Lua", null);
+    const library: string[] | undefined = config.get("workspace.library");
+    if (library && extensionPath) {
+        // remove any older versions of our path e.g. "publisher.name-0.0.1"
+        for (let i = library.length - 1; i >= 0; i--) {
+            const el = library[i];
+            const isSelfExtension = el.indexOf(extensionId) > -1;
+            const isCurrentVersion = el.indexOf(extensionPath) > -1;
+            if (isSelfExtension && !isCurrentVersion) {
+                library.splice(i, 1);
+            }
+        }
+        const index = library.indexOf(folderPath);
+        if (enable) {
+            if (index === -1) {
+                library.push(folderPath);
+            }
+        }
+        else {
+            if (index > -1) {
+                library.splice(index, 1);
+            }
+        }
+        config.update("workspace.library", library, true);
+    }
+}
+
+function updateConfig() {
+    const ourfilesize = 120;
+
+    const sumneko = vscode.workspace.getConfiguration("Lua");
+    const currentSize: number = sumneko.get("workspace.preloadFileSize") || 150;
+    if (currentSize < ourfilesize) {
+        // increase preloadFileSize to make sure our config gets loaded
+        sumneko.update("workspace.preloadFileSize", ourfilesize, true);
+        console.log(`Bumping Lua preloadFileSize to ${ourfilesize}`);
+    }
+}
--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -8,8 +8,4 @@ import * as vscode from 'vscode';
 suite('Extension Test Suite', () => {
 	vscode.window.showInformationMessage('Start all tests.');
 
-	test('Sample test', () => {
-		assert.strictEqual(-1, [1, 2, 3].indexOf(5));
-		assert.strictEqual(-1, [1, 2, 3].indexOf(0));
-	});
 });
