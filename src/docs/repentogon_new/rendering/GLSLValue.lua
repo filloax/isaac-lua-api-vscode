d5c74fe08bc8b746ff83fe003ffa1455d0eb9ac4
Sanio46 <brude602@gmail.com>
2024-04-03 14:26:52 -0400

    GLSLValue

--- src/docs/repentogon_new/rendering/GLSLValue.lua
+++ src/docs/repentogon_new/rendering/GLSLValue.lua
@@ -1,1 +1,11 @@
 ---@class GLSLValue
+local GLSLValue = {}
+
+---meta function to access data in a given table key.
+function GLSLValue:__index()
+end
+
+---meta function to write data in a given table key.
+---@param value number
+function GLSLValue:__newindex(value)
+end
