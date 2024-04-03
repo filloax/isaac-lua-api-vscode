484e28a44480c8746c124b8e1eec7c1b356f8874
Sanio46 <brude602@gmail.com>
2024-04-03 14:19:49 -0400

    Small adjustment to ModRef

--- src/docs/vanilla/ModReference.lua
+++ src/docs/vanilla/ModReference.lua
@@ -5,25 +5,41 @@ local ModReference = {}
 ---@param callbackID ModCallbacks
 ---@param callbackFn function
 ---@param entityID? integer
-function ModReference:AddCallback(callbackID, callbackFn, entityID) end
+function ModReference:AddCallback(callbackID, callbackFn, entityID)
+end
 
 ---@param callbackID ModCallbacks
 ---@param priority CallbackPriority
 ---@param callbackFn function
 ---@param entityID? integer
-function ModReference:AddPriorityCallback(callbackID, priority, callbackFn, entityID) end
+function ModReference:AddPriorityCallback(callbackID, priority, callbackFn, entityID)
+end
 
 ---@return boolean
-function ModReference:HasData() end
+function ModReference:HasData()
+end
 
 ---@return string
-function ModReference:LoadData() end
+function ModReference:LoadData()
+end
 
 ---@param callbackID integer
 ---@param callbackFn function
-function ModReference:RemoveCallback(callbackID, callbackFn) end
+function ModReference:RemoveCallback(callbackID, callbackFn)
+end
 
-function ModReference:RemoveData() end
+function ModReference:RemoveData()
+end
 
 ---@param data string
-function ModReference:SaveData(data) end
+function ModReference:SaveData(data)
+end
+
+-- Disabled as it will complain about mod tables with added functions
+-- in mods with more files, depending on how they are handled; you can
+-- re-enable it and keep it if you don't have that problem
+--[[ ---@param modname string
+---@param apiversion number
+---@return ModReference
+function RegisterMod(modname, apiversion)
+end ]]
