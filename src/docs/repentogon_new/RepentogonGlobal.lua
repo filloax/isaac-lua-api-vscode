076c5d70b9a4b29ffeb9f571e0609519874e7fcf
Sanio46 <brude602@gmail.com>
2024-05-16 16:05:16 -0400

    Convert REPENTOGON definitions to use classes and fields

--- src/docs/repentogon_new/RepentogonGlobal.lua
+++ src/docs/repentogon_new/RepentogonGlobal.lua
@@ -1,3 +1,8 @@
+---@class REPENTOGON
+---@field Version string
+---@field Real true
+---@field Name "REPENTOGON"
+---@field Extras REPENTOGON_Extras
 _G.REPENTOGON = {}
 
 ---Checks if REPENTOGON meets specified version. Always returns `true` on dev builds.
@@ -5,55 +10,47 @@ _G.REPENTOGON = {}
 ---@return boolean
 function REPENTOGON.MeetsVersion(targetVersion) end
 
-REPENTOGON.Real = true
+---@class REPENTOGON_Extras
+---@field Changelog REPENTOGON_Changelog
+---@field StatsMenu REPENTOGON_StatsMenu
+---@field BestiaryMenu REPENTOGON_BestiaryMenu
+---@field Misc {NoRPTGNFldrErr: function}
 
-REPENTOGON.Name = "REPENTOGON"
+---@class REPENTOGON_Changelog
+---@field AssetsLoaded boolean
+---@field CurrentSheet integer
+---@field CurrentState boolean
+---@field ChangelogSprite Sprite
+---@field Font Font
+---@field FontColor KColor
+---@field LineHeight number
+---@field NoteOffset Vector
+---@field NoteSprite Sprite
+---@field PaperOffset Vector
+---@field Sheets {Text: string, TextArray: string[]}[]
+---@field ScrollSpeed number
+---@field ScrollItertia number
+---@field VersionFont Font
+---@field VersionOffset Vector
+---@field EvaluateText function
+---@field LoadAssets function
+---@field MenuRender function
 
-REPENTOGON.Version = nil ---@type string
+---@class REPENTOGON_StatsMenu
+---@field ArrowSprite Sprite
+---@field CurrentlyDisplayedCategoryID integer
+---@field LeftArrowSpritePos Vector
+---@field RightArrowSpritePos Vector
+---@field ScheduleRefresh boolean
+---@field StatPageAngleOffsetX number
+---@field StatPageNumberOffsetX number
+---@field StatSheetSprite Sprite
+---@field StatSheetSpritePos Vector
+---@field Stats [string,[string, number|string|fun():number|string][]][]
 
-REPENTOGON.Extras = {Misc = {}}
-
-REPENTOGON.Extras.ChangeLog = {}
-
-REPENTOGON.Extras.ChangeLog.AssetsLoaded = nil ---@type boolean
-REPENTOGON.Extras.ChangeLog.CurrentSheet = nil ---@type integer
-REPENTOGON.Extras.ChangeLog.CurrentState = nil ---@type boolean
-REPENTOGON.Extras.ChangeLog.ChangelogSprite = nil ---@type Sprite
-REPENTOGON.Extras.ChangeLog.Font = nil ---@type Font
-REPENTOGON.Extras.ChangeLog.FontColor = nil ---@type KColor
-REPENTOGON.Extras.ChangeLog.LineHeight = nil ---@type number
-REPENTOGON.Extras.ChangeLog.NoteOffset = nil ---@type Vector
-REPENTOGON.Extras.ChangeLog.NoteSprite = nil ---@type Sprite
-REPENTOGON.Extras.ChangeLog.PaperOffset = nil ---@type Vector
-REPENTOGON.Extras.ChangeLog.Sheets = nil ---@type {Text: string, TextArray: string[]}[]
-REPENTOGON.Extras.ChangeLog.ScrollSpeed = nil ---@type number
-REPENTOGON.Extras.ChangeLog.ScrollItertia = nil ---@type number
-REPENTOGON.Extras.ChangeLog.VersionFont = nil ---@type Font
-REPENTOGON.Extras.ChangeLog.VersionOffset = nil ---@type Vector
-
-function REPENTOGON.Extras.ChangeLog.EvaluateText() end
-function REPENTOGON.Extras.ChangeLog.LoadAssets() end
-function REPENTOGON.Extras.ChangeLog.MenuRender() end
-
-REPENTOGON.Extras.StatsMenu = {}
-
-REPENTOGON.Extras.StatsMenu.ArrowSprite = nil ---@type Sprite
-REPENTOGON.Extras.StatsMenu.CurrentlyDisplayedCategoryID = nil ---@type integer
-REPENTOGON.Extras.StatsMenu.LeftArrowSpritePos = nil ---@type Vector
-REPENTOGON.Extras.StatsMenu.RightArrowSpritePos = nil ---@type Vector
-REPENTOGON.Extras.StatsMenu.ScheduleRefresh = nil ---@type boolean
-REPENTOGON.Extras.StatsMenu.StatPageAngleOffsetX = nil ---@type number
-REPENTOGON.Extras.StatsMenu.StatPageNumberOffsetX = nil ---@type number
-REPENTOGON.Extras.StatsMenu.StatSheetSprite = nil ---@type Sprite
-REPENTOGON.Extras.StatsMenu.StatSheetSpritePos = nil ---@type Vector
-REPENTOGON.Extras.StatsMenu.Stats = nil ---@type [string,[string, number|string|fun():number|string][]][]
-
-REPENTOGON.Extras.BestiaryMenu = {}
-
-REPENTOGON.Extras.BestiaryMenu.ArrowSprite = nil ---@type Sprite
-REPENTOGON.Extras.BestiaryMenu.BestiarySheetSprite = nil ---@type Sprite
-REPENTOGON.Extras.BestiaryMenu.BestiarySheetSpritePos = nil ---@type Vector
-REPENTOGON.Extras.BestiaryMenu.PageTextPos = nil ---@type Vector
-REPENTOGON.Extras.BestiaryMenu.PageWidgetPos = nil ---@type Vector
-
-function REPENTOGON.Extras.Misc.NoRPTGNFldrErr() end
+---@class REPENTOGON_BestiaryMenu
+---@field ArrowSprite Sprite
+---@field BestiarySheetSprite Sprite
+---@field BestiarySheetSpritePos Vector
+---@field PageTextPos Vector
+---@field PageWidgetPos Vector
