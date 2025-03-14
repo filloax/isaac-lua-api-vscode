4dd88816495ba89e3d826bead3202d1aa54f59e6
BenevolusGoat <brude602@gmail.com>
2025-03-14 11:09:18 -0400

    Add SubCharge field to ActiveItemDesc

--- src/docs/vanilla/ActiveItemDesc.lua
+++ src/docs/vanilla/ActiveItemDesc.lua
@@ -3,6 +3,7 @@
 ---@field Charge integer
 ---@field Item CollectibleType
 ---@field PartialCharge number
+---@field SubCharge integer
 ---@field TimedRechargeCooldown integer
 ---@field VarData integer
 local ActiveItemDesc = {}
