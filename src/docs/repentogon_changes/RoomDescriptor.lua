de36e2b5ecd31ede398a603e69997f4fc6f6d7bc
BenevolusGoat <brude602@gmail.com>
2025-03-30 13:34:59 -0400

    Doors and AllowedDoors comments

--- src/docs/repentogon_changes/RoomDescriptor.lua
+++ src/docs/repentogon_changes/RoomDescriptor.lua
@@ -1,24 +1,32 @@
+---@class RoomDescriptorDoors
+
 ---@class RoomDescriptor
----@field Doors {[DoorSlot]: integer} @RoomDescriptorDoors
+---Allows you to check which level grid index each DoorSlot in the room connects to.
+---
+---For example, roomdesc.Doors[DoorSlot.UP0] provides the level grid index that the upwards door would connect to.
+---
+---The value will be -1 if the RoomShape does not allow a door in that slot.
+---
+---Note that this typically provides a valid index even if there is no door present, and even if the room itself does not allow a door in that slot.
+---@field Doors {[DoorSlot]: RoomDescriptorDoors}
+---Returns a bitmask corresponding to which door slots are currently enabled.
+---
+---Doors are typically only included in this bitmask when there is a door currently present, even if the room would allow a door in that slot.
 ---@field AllowedDoors integer
 ---@field BossDeathSeed integer
 local RoomDescriptor_RGON = {}
 
 -- Note: in current emmylua, defining class twice to add fields to it is valid, as done above
 
----TODO: Document me!
 ---@param gridIndex integer
 function RoomDescriptor_RGON:AddRestrictedGridIndex(gridIndex) end
 
----TODO: Document me!
 ---@return EntitiesSaveStateVector
 function RoomDescriptor_RGON:GetGridEntitiesSaveState(gridIndex) end
 
----TODO: Document me!
 ---@return EntitiesSaveStateVector
 function RoomDescriptor_RGON:GetEntitiesSaveState() end
 
----TODO: Document me!
 ---@return integer[]
 function RoomDescriptor_RGON:GetRestrictedGridIndexes() end
 
