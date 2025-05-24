44a099d72a7f6a979d9b5d64f4d471b549276fbf
Guantol-Lemat <guantol.lemat@gmail.com>
2025-05-24 21:14:50 +0200

    Merge RoomConfig and RoomConfigHolder

--- src/docs/repentogon_new/RoomConfig.lua
+++ src/docs/repentogon_new/RoomConfig.lua
@@ -1,7 +1,37 @@
 ---@class RoomConfig
 _G.RoomConfig = {}
 
+---Returns a RoomConfigRoom corresponding to the given arguments.
+---@param seed integer
+---@param reduceWeight boolean If true, the selected room's weight is decreased.
+---@param stage StbType The stage the room belongs to.
+---@param type RoomType
+---@param shape? RoomShape @default: `RoomShape.NUM_ROOMSHAPES`, which allows the function to pick a room regardless of its shape.
+---@param minVariant? integer @default: `-1`.
+---@param maxVariant? integer @default: `-1`
+---@param minDifficulty? integer @default: `0.`
+---@param maxDifficulty? integer @default: `10.`
+---@param requiredDoors? integer @default: `0.`
+---@param subType? integer @default: `-1`.
+---@param mode? integer @default: `-1`. TODO: Document me!
+---@return RoomConfigRoom
+function RoomConfig.GetRandomRoom(seed, reduceWeight, stage, type, shape, minVariant, maxVariant, minDifficulty,
+										maxDifficulty, requiredDoors, subType, mode)
+end
+
+---Returns a RoomConfigRoom corresponding to the given arguments.
+---@param stage StbType
+---@param type RoomType
+---@param variant integer
+---@param difficulty? integer @default: `-1`, pulling from all difficulties.
+---@return RoomConfigRoom
+function RoomConfig.GetRoomByStageTypeAndVariant(stage, type, variant, difficulty)
+end
+
 ---@param StbType StbType
 ---@return RoomConfigStage
 function RoomConfig.GetStage(StbType)
 end
+
+---@alias RoomConfigHolder RoomConfig
+_G.RoomConfigHolder = RoomConfig
