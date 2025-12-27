0f7194c4ce3c40f99903011524c1f3e0ad7852c0
Sanio <BRude602@gmail.com>
2025-12-27 12:14:46 -0700

    Fix ClearStack defaults

--- src/docs/vanilla/HUD.lua
+++ src/docs/vanilla/HUD.lua
@@ -31,14 +31,14 @@ end
 ---@param MainString string
 ---@param SecondaryString? string
 ---@param IsCurseDisplay? boolean @default: `false`
----@param ClearStack? boolean @default: `false`. Repentance+ Exclusive
+---@param ClearStack? boolean @default: `true`. Repentance+ Exclusive
 ---@diagnostic disable-next-line: duplicate-set-field
 function HUD:ShowItemText(MainString, SecondaryString, IsCurseDisplay, ClearStack)
 end
 
 ---@param Player EntityPlayer
 ---@param Item ItemConfigItem
----@param ClearStack? boolean @default: `false`. Repentance+ Exclusive
+---@param ClearStack? boolean @default: `true`. Repentance+ Exclusive
 ---@diagnostic disable-next-line: duplicate-set-field
 function HUD:ShowItemText(Player, Item, ClearStack)
 end
